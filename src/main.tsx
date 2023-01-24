0ce96514db8cfdbe860bbc99b38472805f199dad
Matilda Mared <matildamared@live.se>
2023-01-24 08:24:33 +0100

    add css reset

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Fonts from "./shared/styles/Fonts";
+import Global from "./shared/styles/Global";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Fonts />
+    <Global />
     <App />
   </React.StrictMode>
 );
